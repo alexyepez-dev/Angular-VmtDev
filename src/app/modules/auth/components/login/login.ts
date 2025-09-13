87f74911522b33bc016d8bb856dcc1022102b491
Alex Yepez <alexdyepez@gmail.com>
2025-09-13 04:31:27 -0500

    Login template Config

--- src/app/modules/auth/components/login/login.ts
+++ src/app/modules/auth/components/login/login.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login-component',
-  imports: [],
+  imports: [ReactiveFormsModule /*RouterLink*/],
   template: `
     <!-- From Uiverse.io by themrsami -->
     <div
@@ -17,22 +18,23 @@ import { Component } from '@angular/core';
           class="bg-white p-16 rounded-lg shadow-2xl w-80 relative z-10 transform transition duration-500 ease-in-out"
         >
           <h2 id="form-title" class="text-center text-3xl font-bold mb-10 text-gray-800">Login</h2>
-          <form class="space-y-5">
+          <form [formGroup]="formLogin" (ngSubmit)="click()" class="space-y-5">
             <input
               class="w-full h-12 border border-gray-800 px-3 rounded-lg"
               placeholder="Email"
-              id=""
-              name=""
               type="text"
+              formControlName="email"
+              required
             />
             <input
               class="w-full h-12 border border-gray-800 px-3 rounded-lg"
               placeholder="Password"
-              id=""
-              name=""
               type="password"
+              formControlName="password"
+              required
             />
             <button
+              type="submit"
               class="w-full h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             >
               Sign in
@@ -44,4 +46,20 @@ import { Component } from '@angular/core';
     </div>
   `,
 })
-export class Login {}
+export class Login {
+  private fb = inject(FormBuilder);
+  public formLogin: FormGroup = this.createFormLogin();
+
+  createFormLogin(): FormGroup {
+    return this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  click() {
+    const value = this.formLogin.value;
+
+    console.log(value);
+  }
+}
