3d39eb4ca65d44b0d496fb40d26fb7db05761a5a
Alex Yepez <alexdyepez@gmail.com>
2025-09-13 02:08:25 -0500

    Config angular material and login component creation

---
+++ src/app/modules/auth/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  template: `
+  <div>
+    Hola desde el login
+  </div>
+  `,
+})
+export class Login { }
